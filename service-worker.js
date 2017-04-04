086d275d9f766ec6317163bde1835fccd2b91224
Rodrigo Fernández <fdez.romero@gmail.com>
2017-04-04 10:57:43 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+/**
+ * Check out https://googlechrome.github.io/sw-toolbox/docs/master/index.html for
+ * more info on how to use sw-toolbox to custom configure your service worker.
+ */
+
+
+'use strict';
+importScripts('./sw-toolbox.js');
+
+self.toolbox.options = {
+  debug: false,
+  networkTimeoutSeconds: 30,
+  cache: {
+    name: 'cache',
+    maxAgeSeconds: 60 * 60 * 24 // 1d
+  }
+};
+
+// dynamically cache any other local assets
+self.toolbox.router.any('/*', self.toolbox.networkFirst);
+
+// Don't cache any 3rd party content, as it may not be HTTPS and fail
+self.toolbox.router.default = self.toolbox.networkOnly;
